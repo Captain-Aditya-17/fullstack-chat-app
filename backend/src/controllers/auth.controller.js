eb2172f1aeaf5d3bf469f9bad11c59592af058df
Captain-Aditya-17 <khushimate90@gmail.com>
2025-04-14 21:20:49 +0530

    work done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -47,7 +47,7 @@ export const signIn = async (req, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'Lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,46 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import connectToDb from './db/db.js';
-import { io, server, app } from './lib/socket.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import connectToDb from "./db/db.js";
+import { io, server, app } from "./lib/socket.js";
 
 dotenv.config();
 connectToDb();
 
 app.use(cookieParser());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cors({
-    origin: 'http://localhost:5173',
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://fullstack-chat-app-sruz.onrender.com/",
+    ],
     credentials: true,
-}));
+  })
+);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/message', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')));
-
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html'));
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "..", "..", "frontend", "dist", "index.html")
+    );
+  });
 }
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
